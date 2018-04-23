f9e7a0f64f93ba999a8ce5869a71cecebe9468d5
Grady <Gradyhouston@gmail.com>
2018-04-22 22:00:04 -0400

    pushing mostly working liri-app, spotify broke for some reason

---
+++ keys.js
@@ -0,0 +1,6 @@
+exports.twitterKeys = {
+  consumer_key: 'jvXo7vL37dYd6zCXYtQ9H3uPS',
+  consumer_secret: 'FbLRwCQdB2GCla9rZ7fyVIK5Avl6wqIA5AWBGPuPtivu8ur6ZR',
+  access_token_key: '987094835336220673-yIT852fcFWtDLsmtdm2WAymRhmq0cc2',
+  access_token_secret: 'ZGT7wLzXho7wUBr3dLVinTl0kHWG5j8bmOd9x2YFdAELB',
+}
